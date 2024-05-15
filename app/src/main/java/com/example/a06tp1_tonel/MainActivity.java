0440dff96dabd375532701b32d14b5859bb616ac
iamfyke <fyketonel@gmail.com>
2024-05-16 07:17:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a06tp1_tonel/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.a06tp1_tonel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button startButton = findViewById(R.id.startButton);
+        Button quitButton = findViewById(R.id.quitButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                System.exit(0);
+            }
+        });
+    }
+}
