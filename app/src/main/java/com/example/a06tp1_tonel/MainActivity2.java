a7c6ca7cf4f1caa61b2f952489a4d0f00289c9db
iamfyke <fyketonel@gmail.com>
2024-05-16 07:52:53 +0800

    Game changes

--- app/src/main/java/com/example/a06tp1_tonel/MainActivity2.java
+++ app/src/main/java/com/example/a06tp1_tonel/MainActivity2.java
@@ -3,12 +3,71 @@ package com.example.a06tp1_tonel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private List<Integer> images = new ArrayList<>(Arrays.asList(R.drawable.camel, R.drawable.coala, R.drawable.fox, R.drawable.lion, R.drawable.monkey, R.drawable.wolf, R.drawable.camel, R.drawable.coala, R.drawable.fox, R.drawable.lion, R.drawable.monkey, R.drawable.wolf));
+    private ImageButton[] imageButtons = new ImageButton[9];
+
+    private int cardBack = R.drawable.blackrec;
+    private int clicked = 0;
+    private boolean turnOver = false;
+    private int lastClicked = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        imageButtons[0] = findViewById(R.id.imageButton1);
+        imageButtons[1] = findViewById(R.id.imageButton2);
+        imageButtons[2] = findViewById(R.id.imageButton3);
+        imageButtons[3] = findViewById(R.id.imageButton4);
+        imageButtons[4] = findViewById(R.id.imageButton5);
+        imageButtons[5] = findViewById(R.id.imageButton6);
+        imageButtons[6] = findViewById(R.id.imageButton7);
+        imageButtons[7] = findViewById(R.id.imageButton8);
+        imageButtons[8] = findViewById(R.id.imageButton9);
+
+        Collections.shuffle(images);
+
+        for (int i = 0; i < 9; i++) {
+            imageButtons[i].setBackgroundResource(cardBack);
+            final int finalI = 1;
+            imageButtons[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (!turnOver) {
+                        imageButtons[finalI].setImageResource(images.get(finalI));
+                        if (clicked == 0) {
+                            lastClicked = finalI;
+                        }
+                        clicked++;
+                    }
+
+                    if (clicked == 2) {
+                        turnOver = true;
+                        if (imageButtons[finalI].getDrawable().getConstantState().equals(imageButtons[lastClicked].getDrawable().getConstantState())) {
+                            imageButtons[finalI].setClickable(false);
+                            imageButtons[lastClicked].setClickable(false);
+                            turnOver = false;
+                            clicked = 0;
+                        }
+                        else if (clicked == 0) {
+
+                        }
+                    }
+                }
+            });
+        }
+
     }
 }
